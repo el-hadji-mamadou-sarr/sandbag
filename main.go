fdda911e7a7f3f28c008362f91e6978683a9d4f7
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2026-01-12 21:04:14 +0100

    proxy + kubernetes

--- main.go
+++ main.go
@@ -2,53 +2,82 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 func backend() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello from backend"))
 	})
-	http.ListenAndServe(":8081", nil)
+	go func() {
+		http.ListenAndServe(":8081", nil)
+	}()
 }
 
-func readRequest(conn net.Conn, upstream string) (*http.Request, error) {
-	buf := bufio.NewReader(conn)
+func readRequest(buf *bufio.Reader, conn net.Conn, upstream string) (*http.Request, error) {
+	conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+
 	req, err := http.ReadRequest(buf)
 	if err != nil {
 		return nil, err
 	}
+	ctx, cancel := context.WithCancel(context.Background())
+	req = req.WithContext(ctx)
+	go func() {
+		<-req.Context().Done()
+		cancel()
+		conn.Close()
+	}()
 	req.URL.Scheme = "http"
 	req.URL.Host = upstream
-	req.Method = "GET"
+	req.Host = upstream
 	req.RequestURI = ""
 	return req, nil
 }
 
-func forwardRequest(req *http.Request) (*http.Response, error) {
+func forwardRequest(client *http.Client, req *http.Request) (*http.Response, error) {
 	req.Header.Set("X-Proxy-Version", "v1")
 
-	client := &http.Client{}
 	return client.Do(req)
+}
+
+func shouldClose(req *http.Request, res *http.Response) bool {
+	if req.Close || res.Close {
+		return true
+	}
+	return false
 }
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	req, err := readRequest(conn, "localhost:8081")
-	if err != nil {
-		fmt.Println("error while reading request:", err)
-		return
+	buf := bufio.NewReader(conn)
+	upstreamClient := &http.Client{
+		Timeout: 0,
 	}
-	res, err := forwardRequest(req)
-	if err != nil {
-		fmt.Println("error while forwarding response: ", err)
-		return
+	for {
+		req, err := readRequest(buf, conn, "localhost:8081")
+		if err != nil {
+			fmt.Println("error while reading request:", err)
+			return
+		}
+		res, err := forwardRequest(upstreamClient, req)
+		if err != nil {
+			fmt.Println("error while forwarding response: ", err)
+			return
+		}
+		if err := res.Write(conn); err != nil {
+			return
+		}
+		if shouldClose(req, res) {
+			return
+		}
+
 	}
-	defer res.Body.Close()
-	res.Write(conn)
 }
 
 func main() {
